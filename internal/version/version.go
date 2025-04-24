23d13900ab3057f0e611ecaf67a6e1b925ef34e6
Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>
2025-04-24 18:07:35 +0300

    Add copyright header
    Signed-off-by: Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,7 @@
+// © Broadcom. All Rights Reserved.
+// The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: MPL-2.0
+
 package version
 
 // ProviderVersion is set during the release process to the release version of the binary
